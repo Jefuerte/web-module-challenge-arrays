e4bdb98e6bd291d8dfbdad0a3198dbe54962044d
jessica <fuerte.jessica10@gmail.com>
2021-11-03 14:50:29 -0500

    solved task two and three

--- index.js
+++ index.js
@@ -45,11 +45,12 @@ Use the copy function below to do the following:
   2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-  /*your code here*/
+function copy(array){
+  let copy = [...originalFlavors]
+  return array;
 }    
 
-
+console.log(copy)
 
 
 
@@ -64,9 +65,13 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
- /*your code here*/
+function is31Flavors(arr){
+ if(originalFlavors.length === 31){
+   return true;
+ }
 }
+console.log(is31Flavors)
+
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
@@ -81,8 +86,10 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
- /*your code here*/
+function addFlavor(arr, newFlavor){
+ arr.unshift(newFlavor);
+   return arr;
+ 
 }
 
 
